019122ffd16f567b74ef542e2c2e63be96d90a65
Jérôme Tchan <jerome.tchan@epita.fr>
2019-11-12 10:43:35 +0100

    Code cleanup

--- nightcore.py
+++ nightcore.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3
 
 from pydub import AudioSegment
-from pydub import effects
-
 from random import randrange, choice
 from xml.etree import ElementTree
 
@@ -14,17 +12,21 @@ import subprocess
 import argparse
 import youtube_dl
 
+
 def speedup_song(path):
     sound = AudioSegment.from_file(path)
-    
-    octaves = 4/12 # x1.25
+
+    octaves = 4 / 12  # x1.25
     new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
     nightcore = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate}).set_frame_rate(44100)
 
     nightcore.export("tmp/nightcore.mp3", format="mp3", bitrate="192k")
 
+
 def get_request_url(page, tag):
-    return "https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=" + str(page) + "&tags=width:1920+height:1080+-swimsuit+-feet+-text+score:>=1+" + tag
+    return "https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=" + str(
+        page) + "&tags=width:1920+height:1080+-swimsuit+-feet+-text+score:>=1+" + tag
+
 
 def get_random_image():
     tags = ['looking_at_another', '1girl', 'vocaloid', '1girl+1boy', '2girls']
@@ -45,12 +47,15 @@ def get_random_image():
     with open('tmp/image.jpg', 'wb') as handler:
         handler.write(img_data)
 
+
 def render_video(path, singleframe):
     rate = ''
     if singleframe:
         rate = '-r 1 '
-    cmd = 'ffmpeg -loop 1 -i tmp/image.jpg -i tmp/nightcore.mp3 -c:v libx264 ' + rate + '-c:a copy -pix_fmt yuv420p -shortest ' + path
+    cmd = 'ffmpeg -loop 1 -i tmp/image.jpg -i tmp/nightcore.mp3 -c:v libx264 ' + rate + '-c:a copy -pix_fmt yuv420p ' \
+                                                                                        '-shortest ' + path
     return subprocess.call(cmd, shell=True)
+
 
 def youtube_download(terms):
     ydl_opts = {
@@ -65,15 +70,17 @@ def youtube_download(terms):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([terms])
 
+
 def main(args):
-    if (os.path.isdir('tmp')):
+    if os.path.isdir('tmp'):
         shutil.rmtree('tmp')
     os.mkdir('tmp')
 
+    path = ''
     if args.ytdl:
         youtube_download(args.ytdl)
         path = 'tmp/youtubedl.mp3'
-    
+
     elif args.search:
         youtube_download('ytsearch:' + args.search)
         path = 'tmp/youtubedl.mp3'
@@ -87,7 +94,7 @@ def main(args):
     output = "nightcore.mp4"
     if args.output:
         output = args.output
-    
+
     return_code = render_video(output, args.frame)
     shutil.rmtree('tmp')
 
@@ -96,12 +103,18 @@ def main(args):
     else:
         print('Error while rendering video...', file=sys.stderr)
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A tool to automatically generate nightcore videos out of an audio file. You should be ashamed of yourself for using that.")
+    parser = argparse.ArgumentParser(
+        description="A tool to automatically generate nightcore videos out of an audio file. You should be ashamed of "
+                    "yourself for using that.")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-y", "--ytdl", help="use youtube-dl to search for a video or download a specific video")
-    group.add_argument("-s", "--search", help="search for a specific song to Nightcore-ify on YouTube (same as --ytdl ytsearch:[search])")
+    group.add_argument("-s", "--search",
+                       help="search for a specific song to Nightcore-ify on YouTube (same as --ytdl ytsearch:[search])")
     group.add_argument("-f", "--file", help="file path to the song to Nightcore-ify")
     parser.add_argument("-o", "--output", help="name of the output file")
-    parser.add_argument("-1", "--frame", action='store_true', help="sets framerate to 1 for faster encoding & lighter file. May affect compatibility with some video players")
+    parser.add_argument("-1", "--frame", action='store_true',
+                        help="sets framerate to 1 for faster encoding & lighter file. May affect compatibility with "
+                             "some video players")
     main(parser.parse_args())
