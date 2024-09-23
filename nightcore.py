0b564aa0d8c9bf8bf900c50cfc4fb12dc36ae0e0
TheDiamondOG <91386453+TheDiamondOG@users.noreply.github.com>
2024-09-23 13:38:20 -0400

    Added in Debugging, FFmpeg library support, and filenames using the original filename

--- nightcore.py
+++ nightcore.py
@@ -11,10 +11,11 @@ import requests
 import subprocess
 import argparse
 import yt_dlp
+import ffmpeg
 
 pid = os.getpid()
 tmp_dir = "tmp_" + str(pid)
-
+debug = False
 
 def speedup_song(path):
     sound = AudioSegment.from_file(path)
@@ -22,6 +23,8 @@ def speedup_song(path):
     octaves = 4 / 12  # x1.25
     new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
     nightcore = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate}).set_frame_rate(44100)
+
+    print("Exporting Audio")
 
     nightcore.export(tmp_dir + "/nightcore.mp3", format="mp3", bitrate="192k")
 
@@ -50,14 +53,43 @@ def get_random_image():
     with open(tmp_dir + '/image.jpg', 'wb') as handler:
         handler.write(img_data)
 
+    print("Grabbed Image: "+ image_url)
 
 def render_video(path, singleframe):
-    rate = ''
+    input_image = tmp_dir + '/image.jpg'
+    input_audio = tmp_dir + '/nightcore.mp3'
+    
+    video = ffmpeg.input(input_image, loop=1)
+    audio = ffmpeg.input(input_audio)
+    
     if singleframe:
-        rate = '-r 1 '
-    cmd = 'ffmpeg -loop 1 -i ' + tmp_dir + '/image.jpg -i ' + tmp_dir + '/nightcore.mp3 -c:v libx264 ' + rate + '-c:a copy -pix_fmt yuv420p ' \
-                                                                                                                '-shortest ' + path
-    return subprocess.call(cmd, shell=True)
+        video = ffmpeg.filter(video, 'fps', fps=1)
+    
+    output_params = {
+        'vcodec': 'libx264',
+        'acodec': 'copy',
+        'pix_fmt': 'yuv420p',
+        'shortest': None
+    }
+    
+    try:
+        if debug == True:
+            (
+                ffmpeg
+                .output(video, audio, path, **output_params)
+                .run(quiet=False, overwrite_output=True)
+            )
+        if debug == False:
+            (
+                ffmpeg
+                .output(video, audio, path, **output_params)
+                .run(quiet=True, overwrite_output=True)
+            )
+        return True
+    except ffmpeg.Error as e:
+        print("Error when making the video: \n"+e.stderr.decode('utf8'))
+        return False
+
 
 
 def youtube_download(terms):
@@ -75,49 +107,69 @@ def youtube_download(terms):
 
 
 def main(args):
+    global debug
+    if args.debug:
+        debug = True
+        print("Debugging Enabled")
+
     if os.path.isdir(tmp_dir):
         shutil.rmtree(tmp_dir)
+        print("Removed old temp directory")
     os.mkdir(tmp_dir)
+    print("Made temp directory")
 
     path = ''
     if args.ytdl:
+        print("Started Video Downloading")
         youtube_download(args.ytdl)
         path = tmp_dir + '/yt_dlp.mp3'
+        print("Finished Downloading")
 
     elif args.search:
+        print("Started search with "+ args.search)
         youtube_download('ytsearch:' + args.search)
         path = tmp_dir + '/yt_dlp.mp3'
+        print("Finished search")
 
     elif args.file:
+        print("Set path is "+ args.file)
         path = args.file
 
+    print("Starting song speedup")
     speedup_song(path)
+
+    print("Grabbing Image from SafeBooru")
     get_random_image()
 
-    output = "nightcore_" + str(pid) + ".mp4"
+    print("Grabbing Filename from Path")
+    if "/" in path:
+        output = path.split("/")[-1] + " Nightcore" + ".mp4"
+    if "\\" in path:
+        output = path.split("\\")[-1] + " Nightcore"+ ".mp4"
     if args.output:
         output = args.output
 
+    print("Output directory is "+ output)
+
+    print("Rendering video")
     return_code = render_video(output, args.frame)
+
+    print("Removing temp directory")
     shutil.rmtree(tmp_dir)
 
-    if return_code == 0:
+    if return_code == True:
         print('Nightcore video successfully generated! You should be ashamed of yourself.')
     else:
         print('Error while rendering video...', file=sys.stderr)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="A tool to automatically generate nightcore videos out of an audio file. You should be ashamed of "
-                    "yourself for using that.")
+    parser = argparse.ArgumentParser(description="A tool to automatically generate nightcore videos out of an audio file. You should be ashamed of yourself for using that.")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-y", "--ytdl", help="use yt-dlp to search for a video or download a specific video")
-    group.add_argument("-s", "--search",
-                       help="search for a specific song to Nightcore-ify on YouTube (same as --ytdl ytsearch:[search])")
+    group.add_argument("-s", "--search", help="search for a specific song to Nightcore-ify on YouTube (same as --ytdl ytsearch:[search])")
     group.add_argument("-f", "--file", help="file path to the song to Nightcore-ify")
     parser.add_argument("-o", "--output", help="name of the output file")
-    parser.add_argument("-1", "--frame", action='store_true',
-                        help="sets framerate to 1 for faster encoding & lighter file. May affect compatibility with "
-                             "some video players")
+    parser.add_argument("-1", "--frame", action='store_true', help="sets framerate to 1 for faster encoding & lighter file. May affect compatibility with some video players")
+    parser.add_argument("-d", "--debug", action='store_true', help="Running the script with the debug output")
     main(parser.parse_args())
